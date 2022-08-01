ee58dfbbe7e5919ac51e6ebe415388f28f8313db
owuraku-zenas <awukuzenas@gmail.com>
2022-08-01 20:52:05 +0000

    Connection Page Completed

--- src/views/ConnectionView.jsx
+++ src/views/ConnectionView.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../styles/ConnectionView.module.css'
 
 const ConnectionView = ({ connect, errorMessage }) => {
-    const ip = "localhost";
-    const port = "9090";
+  const [ip, setIp] = useState("");
+  const [port, setPort] = useState("");
 
   return (
-    <div>
-        <header className="App-header">
-            <h1>ROS UI</h1>
-            {errorMessage && <p>{errorMessage}</p>}
-            {/* TODO: Input Field for ip and port*/}
-            
-            <button onClick={() => connect(ip, port)} >Connect</button>
-          </header>
+    <div className={styles.connection__area}>
+      <h1>ROS UI</h1>
+      {errorMessage && <p className={styles.error__message}>{errorMessage}</p>}
+      {/* TODO: Input Field for ip and port*/}
+      <form className={styles.form} onSubmit={
+        (event) => {
+          event.preventDefault()
+          connect(ip, port)
+        }}>
+          <input
+            type="text"
+            value={ip}
+            onChange={
+              (event) => {
+                setIp(event.target.value)
+              }
+            }
+            placeholder="IP Address"
+            required
+          />
+          <input type="text"
+            value={port}
+            placeholder="Port Number"
+            min={80}
+            required
+            max={9999}
+            onChange={
+              (event) => {
+                setPort(event.target.value)
+              }
+            }
+          />
+        <button className={styles.connect__button}>Connect</button>
+      </form>
     </div>
   )
 }
