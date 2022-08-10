110ae49a2ac76470f0ae68b248cf15c842e734a0
owuraku-zenas <awukuzenas@gmail.com>
2022-08-10 12:30:41 +0000

    Sign out Implementation

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import styles from '../styles/SideBar.module.css'
 import control from '../images/control.png'
 import logo from '../images/ros-logo.png'
@@ -8,10 +8,31 @@ import { GiCube } from 'react-icons/gi'
 import { IoLogoModelS } from 'react-icons/io'
 import { GoSignOut } from 'react-icons/go'
 import { FaThList, FaUserCog } from 'react-icons/fa'
+import AuthContext from '../context/AuthProvider'
+import { logout } from '../api/services'
 
 
 const SideBar = () => {
     const [open, setOpen] = useState(false)
+    const { setAuth } = useContext(AuthContext)
+    const { setRos } = useContext(AuthContext)
+
+    // Create a signout function
+    const signout = async () => {
+        const token = localStorage.getItem('token')
+        const response = await logout(token)
+        if (response?.status === 200) {
+            setAuth(null)
+            setRos(null)
+            localStorage.removeItem('token')
+            localStorage.removeItem('rosURL')
+            window.location.reload(false);
+        } else {
+            console.log("Error signing out")
+            alert("Error signing out")
+        }
+    }
+
     const Menu = [
         { title: "Dashboard", icon: <MdSpaceDashboard size={30} />, link: "/dashboard" },
         { title: "GPS", icon: <MdLocationOn size={30} />, link: "/gps" },
@@ -53,7 +74,9 @@ const SideBar = () => {
                     ))}
                 </ul>
                 <ul className={styles.menu__list}>
-                    <li className={styles.menu__item}>
+                    <li className={styles.menu__item} onClick={() => {
+                        signout()
+                    }}>
                         <GoSignOut size={30}/>
                         <span style={!open ? { display: "none" } : null} >Sign Out</span>
                     </li>
