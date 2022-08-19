aa3ebcdba5addcf13426afd800c0e6fdc30c9240
owuraku-zenas <awukuzenas@gmail.com>
2022-08-19 03:11:24 +0000

    removed some starts from context api

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -5,20 +5,17 @@ const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState(null);
     const [ros, setRos] = useState(null);
-    const [topics, setTopics] = useState(null);
     const [map, setMap] = useState(null);
 
-    // TODO: Add new states to the reset function
     const resetContext = () => {
         localStorage.removeItem('token')
         localStorage.removeItem('rosURL')
         setAuth(null)
         setRos(null)
-        setTopics(null)
     }
 
     return (
-        <AuthContext.Provider value={{ auth, setAuth, setRos, ros, topics, setTopics, resetContext, map, setMap }}>
+        <AuthContext.Provider value={{ auth, setAuth, setRos, ros, resetContext, map, setMap }}>
             {children}
         </AuthContext.Provider>
     );
